b69be381af461e1283fe8b4200ea05050a882234
Adrián Salas Troya <45205815+adrisalas@users.noreply.github.com>
2019-12-03 20:15:59 +0100

    Login Query

--- electron-project/index.js
+++ electron-project/index.js
@@ -1,9 +1,14 @@
 function ok() {
     var output = '';
+    var usuario = document.getElementById("usuario").value;
+    var password = document.getElementById("password").value;
+    console.log("\n user: " + usuario + "\n pass:" + password);
+    $query = "SELECT * FROM practicaGI.tUsuario WHERE nif = '" + 
+    usuario + "' AND password = '" + password + "';";
 
-    select("SELECT * FROM tUsuario;",function(result){
+    select($query,function(result){
         output = result;
-        console.log(output);
+        console.table(output);
     });
 
     alert("Vamos a iniciar sesion");  
