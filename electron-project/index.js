1cdf344aae35b1b64baa61be01b8bb0283260b4c
Adrián Salas Troya <45205815+adrisalas@users.noreply.github.com>
2019-12-04 14:58:58 +0100

    FUNCIONAAAA

--- electron-project/index.js
+++ electron-project/index.js
@@ -13,6 +13,7 @@ function ok() {
         console.table(output);
         if(output[0] != undefined){
             alert("ABRIR VENTANA");
+	location.replace('muestras.html');
         } else {
             alert("Usuario incorrecto");      
         }
